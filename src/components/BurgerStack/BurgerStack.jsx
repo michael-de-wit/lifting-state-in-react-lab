112222aa90fe9069e71e5dec738294966211ddb5
michael-de-wit <michael.b.dewit@gmail.com>
2025-11-05 17:46:07 -0800

    mid ingredient list

---
+++ src/components/BurgerStack/BurgerStack.jsx
@@ -0,0 +1,9 @@
+// src/components/BurgerStack/BurgerStack.jsx
+
+import { useState } from 'react';
+
+const BurgerStack = (props) => {
+  return <ul>// map through props.ingredients</ul>;
+};
+
+export default BurgerStack;
---
+++ src/components/IngredientList/IngredientList.jsx
@@ -0,0 +1,36 @@
+// src/components/IngredientList/IngredientList.jsx
+
+import { useState } from 'react';
+
+const IngredientList = (props) => {
+    
+    const handleAdd = (event) => {
+        event.preventDefault();
+        console.log(`add ok`);
+        props.addIngredient(ingredient)
+    }
+
+    const[ingredientToAdd, setIngredientToAdd]=useState('');
+
+    return (
+        <ul>
+            {props.availableIngredients.map((ingredient, index) => {
+                return (
+                    <>
+                        <form onSubmit={handleAdd}>
+                            <li 
+                                key={index} 
+                                style={{ backgroundColor: ingredient.color }}
+                                value={ingredientToAdd}>
+                                    {ingredient.name}
+                                <button type="submit" style={{ color: 'white' }}>+</button>
+                            </li>
+                        </form>
+                    </>
+                )
+            })}
+        </ul>
+    )
+};
+
+export default IngredientList;
